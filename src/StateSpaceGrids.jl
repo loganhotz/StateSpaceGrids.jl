73beeee18c3f9c210e69265505dd45a26ca90cf7
hotzsauce <githotz@gmail.com>
2022-07-29 19:18:36 -0400

    subtyped dimensions & grids

--- src/StateSpaceGrids.jl
+++ src/StateSpaceGrids.jl
@@ -11,12 +11,13 @@ using Reexport
 
 
 
-# abstract grids
+# abstract grids and meshes
 export AbstractMesh
 export AbstractGrid
 
-# concrete grids
+# concrete grids and meshes
 export Grid
+export Mesh
 
 # abstract dimensions
 export AbstractStateSpaceDimension
@@ -25,25 +26,34 @@ export AbstractGridDimension
 
 # concrete dimensions
 export GridDimension
+export MeshDimension
+
+# instantiation functions
+export grid
+export mesh
 
 # creating state-space solutions, and an `Interpolations.jl` function with methods that
 #   interact with AbstractMesh structs
 export allocate_solution
 export interpolate
 
+# utils for meshes, grids, and dimensions
+export dims
+export steps
 
 
-abstract type AbstractMesh{T, N} end
-abstract type AbstractGrid{T, N} <: AbstractMesh{T, N} end
 
-abstract type AbstractStateSpaceDimension{T<:Number} end
+abstract type AbstractMesh{T<:Real, N} <: AbstractArray{T, N} end
+abstract type AbstractGrid{T, N}       <: AbstractMesh{T, N} end
+
+abstract type AbstractStateSpaceDimension{T<:Real} <: AbstractVector{T} end
 abstract type AbstractMeshDimension{T} <: AbstractStateSpaceDimension{T} end
 abstract type AbstractGridDimension{T} <: AbstractStateSpaceDimension{T} end
 
 
+
 include("dimensions.jl")
 include("grids.jl")
-include("indices.jl")
 include("interpolations.jl")
 include("solutions.jl")
 
